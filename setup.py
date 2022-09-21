dd70ee280c06f2f64fea428ad5bfa4dc7f0a61fc
Gabriel Rodrigues Palma <gabrielrpalma2@gmail.com>
2022-09-21 08:31:38 +0100

    Bump version: 0.0.8 → 0.0.9

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/GabrielRPalma/pypbp',
-    version='0.0.8',
+    version='0.0.9',
     zip_safe=False,
     ext_modules=cythonize("pypbp/__init__.pyx"),
 )
